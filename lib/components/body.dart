8301fdfcf7ebf268acc4f577189280b4c20dd27e
samuelmozarthss <contatosamuelm@gmail.com>
2022-06-03 01:33:22 -0300

    Add inputField

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -23,7 +23,7 @@ class Body extends StatelessWidget {
               begin: FractionalOffset.topCenter,
               end: FractionalOffset.bottomCenter,
               colors: [
-                Colors.black.withOpacity(0.9),
+                Colors.black.withOpacity(0.7),
                 Colors.black,
               ],
             ),
@@ -31,24 +31,85 @@ class Body extends StatelessWidget {
         ),
         Stack(
           children: <Widget>[
-            Container(
-              alignment: Alignment.center,
-              child: Image.asset(
-                'assets/images/burger.png',
-                height: 100,
-                width: 100,
-              ),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                'FastFood',
-                style: TextStyle(fontSize: 30, color: Colors.white),
-              ),
-            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    'assets/images/burger.png',
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'FastFood',
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                inputField(context),
+                SizedBox(
+                  height: 30,
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    textStyle: TextStyle(
+                      color: Color.fromRGBO(255, 255, 255, 1),
+                    ),
+                  ),
+                  onPressed: null,
+                  child: Text('Or Continue With'),
+                ),
+              ],
+            )
           ],
         ),
       ],
     );
   }
+}
+
+inputField(context) {
+  return Column(
+    children: [
+      TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.only(top: 20, bottom: 20, left: 40),
+          hintText: "Email",
+          hintStyle: TextStyle(
+            color: Color.fromRGBO(81, 81, 81, 1),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(18),
+            borderSide: BorderSide.none,
+          ),
+          fillColor: Color.fromRGBO(37, 37, 37, 1),
+          filled: true,
+        ),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.only(top: 20, bottom: 20, left: 40),
+          hintText: "Password",
+          hintStyle: TextStyle(
+            color: Color.fromRGBO(81, 81, 81, 1),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: BorderSide.none,
+          ),
+          fillColor: Color.fromRGBO(37, 37, 37, 1),
+          filled: true,
+        ),
+      ),
+    ],
+  );
 }
